7e8d8cba1a8d970698078f219cb2c8ffe0b7e84d
tj-xubuntu <abongsjoel@gmail.com>
2020-11-26 21:25:22 +0100

    Remove the first three elements of an array

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -41,7 +41,7 @@ end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-
+  array.shift(3)
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
 end
--- ruby_basics/6_arrays/spec/array_exercises_spec.rb
+++ ruby_basics/6_arrays/spec/array_exercises_spec.rb
@@ -84,11 +84,11 @@ RSpec.describe 'Array Exercises' do
 
   describe 'remove first three elements exercise' do
 
-    xit 'returns the array without the first three elements' do
+    it 'returns the array without the first three elements' do
       expect(remove_first_three_elements([1, 3, 5, 7, 9])).to eq([7, 9])
     end
 
-    xit 'returns an empty array when the array has less than 3 elements' do
+    it 'returns an empty array when the array has less than 3 elements' do
       expect(remove_first_three_elements(['foo', 'bar'])).to eq([])
     end
   end
